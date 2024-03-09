c0b6b2f4b2d6099c94de391425ba45a030f7d226
shota-nagato <shota.nagato@gmail.com>
2024-03-09 17:32:03 +0900

    articleモデルのテスト追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
   belongs_to :user
   enum :status, { unsaved: 10, draft: 20, published: 30 }, _prefix: true
 
-  validates :title, comments, presence: true, if: :published?
+  validates :title, :content, presence: true, if: :published?
+  validate :verify_only_one_unsaved_status_is_allowed
 
   private
 
