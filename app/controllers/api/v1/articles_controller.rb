1201677f186468adcc1c34fe90d24d5e50f50446
shota-nagato <shota.nagato@gmail.com>
2024-03-09 21:02:37 +0900

    記事一覧を返すAPI

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
+  def index
+    pagy, articles = pagy(Article.published.order(created_at: :desc))
+    render json: articles
+  end
+
   def show
     article = Article.published.find(params[:id])
     render json: article
