66e5abd3d0025d9dbcddfdc03561d9418954d1de
shota-nagato <shota.nagato@gmail.com>
2024-03-09 23:50:10 +0900

    pagy修正

--- app/controllers/concerns/paginationable.rb
+++ app/controllers/concerns/paginationable.rb
@@ -3,25 +3,15 @@ require "pagy/extras/headers"
 module Paginationable
   extend ActiveSupport::Concern
 
-  DEFAULT_PAGINATION_LIMIT = 20
-  MAX_PAGINATION_LIMIT = 200
+  LIMIT = 20
 
   included do
     include Pagy::Backend
   end
 
   def pagination(items)
-    pagy, items = pagy(items, items: limit)
+    pagy, items = pagy(items, items: LIMIT)
     pagy_headers_merge(pagy)
     items
-  end
-
-  private
-
-  def limit
-    [
-      params.fetch(:limit, DEFAULT_PAGINATION_LIMIT).to_i,
-      MAX_PAGINATION_LIMIT
-    ].min
   end
 end
