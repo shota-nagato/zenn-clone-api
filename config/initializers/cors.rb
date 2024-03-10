c4d6ec08484662f40b2a66bb69ae0e051594e37b
shota-nagato <shota.nagato@gmail.com>
2024-03-10 14:46:23 +0900

    swaggerのcors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.front_domain
+    origins [Settings.front_domain, Settings.swagger_domain]
 
     resource "*",
       headers: :any,
